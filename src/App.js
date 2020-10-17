cd8ecf9f2bdd32724d9d1eba6f68b27013a61cb3
MuhammadSalmanManzoor <salmanmanzoor1244@gmail.com>
2020-10-17 00:34:55 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,19 +6,21 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          edit <code>src/App.js</code> and save to reload
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
+          href="http://reactjs.org"
+          target="blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
+
       </header>
+     
     </div>
   );
 }
