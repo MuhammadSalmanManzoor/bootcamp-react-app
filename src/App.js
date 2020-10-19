3e7ca4f8dd303bef9b3ced34cdbfc77b3047f62e
MuhammadSalmanManzoor <salmanmanzoor1244@gmail.com>
2020-10-19 04:04:42 -0700

    editing some feature

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
 import './App.css';
-
+import Dinner from './dinner.js';
 function App() {
   return (
-    <div>
-    <h1>Hi Everybody</h1>
+    <div className="APP">
+     {/* <Dinner></Dinner> */}
+     <Dinner dishname="chicken biryani" sweetdish ="kheer" />
+     <hr/>
+     <Dinner dishname="chicken krahi" sweetdish ="jalybi" />
+     <hr/>
+     <Dinner dishname="mutton" sweetdish ="halwa" />
     </div>
   );
 }
---
+++ src/dinner.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+ function Dinner (props) {
+    return (
+        <div>
+            <h1>we surve { props.dishname}</h1>
+            <h1>we surve also { props.sweetdish}</h1>
+            
+        </div>
+    )
+}
+
+export default Dinner
